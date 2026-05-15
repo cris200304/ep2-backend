9b9655cde68cace1784a5dc3d4f3cb7bbc3713db
Cristian Cerda <cristiancerda@MacBook-Air-de-Cristian.local>
2026-05-15 12:09:36 -0400

    agregar gitignore y limpiar node_modules

--- server.js
+++ server.js
@@ -1,20 +1,49 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const mysql = require("mysql2/promise");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
+async function getConnection() {
+  return await mysql.createConnection({
+    host: "db",
+    user: "root",
+    password: "root",
+    database: "ep2db",
+  });
+}
 
 app.get("/", (req, res) => {
   res.json({
-    mensaje: "Backend EP2 funcionando correctamente"
+    mensaje: "Backend + MySQL funcionando"
   });
 });
 
+app.get("/items", async (req, res) => {
+  try {
+    const connection = await getConnection();
+
+    const [rows] = await connection.execute(
+      "SELECT * FROM items"
+    );
+
+    await connection.end();
+
+    res.json(rows);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Error base de datos"
+    });
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Servidor corriendo puerto ${PORT}`);
 });
